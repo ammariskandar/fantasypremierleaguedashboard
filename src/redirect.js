6fea7d9841e39dc7254b22f62208b9bbd26effee
ammariskandar <ammariskandar7@gmail.com>
2024-07-29 16:58:50 +0800

    Initial commit

---
+++ src/redirect.js
@@ -0,0 +1,5 @@
+// redirect.js
+if (window.location.hostname === 'ammariskandar.github.io' && window.location.pathname === '/') {
+    window.location.href = 'https://ammariskandar.github.io/fantasypremierleaguedashboard/';
+  }
+  
